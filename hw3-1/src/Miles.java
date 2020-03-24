58a86451ec598e394b6a87042795cca3c81c01bf
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-03-24 11:38:37 +0500

    change miles.java

--- hw3-1/src/Miles.java
+++ hw3-1/src/Miles.java
@@ -4,15 +4,10 @@ import java.util.Scanner;
 public class Miles {
 
     public static void main(String[] args) {
-        Scanner vvod = new Scanner(System.in);
-        int TicketPrice;
-
+        Scanner input = new Scanner(System.in);
         System.out.print("Введите цену вашего билета: ");
-        TicketPrice = vvod.nextInt();
-        // int MilesNumber = Math.round (TicketPrice / 20);
-
-        int MilesNumber =  (TicketPrice / 20);
-
-        System.out.print("Число миль которое Вам начисленно: " + MilesNumber);
+        int ticketPrice = input.nextInt();
+        int milesNumber = (ticketPrice / 20);
+        System.out.print("Число миль которое Вам начисленно: " + milesNumber);
     }
 }
